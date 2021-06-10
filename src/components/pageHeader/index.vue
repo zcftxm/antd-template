e3d04f49c396018fbb62961673f96cf79fe8b119
张求艺 <2432452330@qq.com>
2021-06-10 14:00:45 +0800

    面包屑

--- src/components/pageHeader/index.vue
+++ src/components/pageHeader/index.vue
@@ -1,45 +1,67 @@
 <template>
   <div class="page-header">
     <a-breadcrumb>
-      <a-breadcrumb-item v-for="(item,index) in titleList" :key="index">{{item}}</a-breadcrumb-item>
-      <!-- <a-breadcrumb-item>List</a-breadcrumb-item>
-      <a-breadcrumb-item>App</a-breadcrumb-item> -->
+      <a-breadcrumb-item v-for="(item, index) in titleList" :key="index">
+        <router-link :to="item.path">{{ item.name }}</router-link>
+      </a-breadcrumb-item>
     </a-breadcrumb>
-    <div class="page-detail">
-      <slot></slot>
-    </div>
+    <div class="page-detail"><slot></slot>{{ detail }}</div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'PageHeader',
+  name: "PageHeader",
   // props: {}
   data() {
     return {
-      titleList:[]
+      titleList: [],
+      detail: "",
       // collapsed: false
     };
   },
-  mounted(){
+  watch: {
+    $route(to) {
+      // console.log(to);
+      let list = [];
+      this.detail = to.meta.title;
+      to.matched.forEach((item) => {
+        let obj = {};
+        obj.name = item.meta.title;
+        obj.path = item.path;
+        list.push(obj);
+      });
+      this.titleList = list;
+    },
+  },
+  mounted() {
     let list =[]
+    this.detail = this.$route.meta.title
     this.$route.matched.forEach(item => {
-      list.push(item.meta.title)
+      let obj = {}
+      obj.name = item.meta.title
+      obj.path = item.path
+      list.push(obj)
     });
-    console.log(list)
     this.titleList = list
-  }
-}
+  },
+};
 </script>
 <style lang="less" scoped>
-.page-header{
+.page-header {
   background: white;
-  height: 40px;
+  height: 70px;
   padding-left: 20px;
   display: flex;
-  align-items: center;
-  .ant-breadcrumb{
+  // align-items: center;
+  flex-direction: column;
+  align-items: end;
+  justify-content: center;
+  .ant-breadcrumb {
     font-size: 16px;
+  }
+  .page-detail {
+    font-size: 24px;
   }
 }
 </style>
