4195dd399b340b5cb1c26fcad3f0090a2101e12d
zcftxm <1126509682@qq.com>
2021-06-11 16:00:56 +0800

    fix: config

--- vue.config.js
+++ vue.config.js
@@ -3,23 +3,13 @@ module.exports = {
   publicPath: './',
   // 全局less文件
   pluginOptions: {
-    "style-resources-loader": { 
-      preProcessor: "less", 
+    "style-resources-loader": {
+      preProcessor: "less",
       patterns: [
         path.resolve(__dirname, "./src/core/global.less")
-      ] 
+      ]
     }
   },
-  chainWebpack: config => {
-    config.module
-     .rule('eslint')
-     .use('eslint-loader')
-     .loader('eslint-loader')
-     .tap(options => {
-     options.fix = true
-     return options
-     })
-    },
   devServer: {
     // development server port 8000
     port: 8000,
